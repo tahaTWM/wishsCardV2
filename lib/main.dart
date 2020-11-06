c0b35fc23e82e83b277cd42bcfc93e592bb3767a
tahaTWM <taha.twm.1234@gmail.com>
2020-11-06 11:01:25 +0300

    3

--- lib/main.dart
+++ lib/main.dart
@@ -5,98 +5,100 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> Names = <String>[
-    'Taha',
-    'TWM',
-    'Tony',
-    'ALi',
-    'John',
-    'Sara',
-    'Ameer',
-  ];
-  final List<String> wishes = <String>[
-    'In 2021 I wanna B Happy',
-    'In 2021 I Wanna B rich',
-    'In 2021 I wanna B Teacher',
-    'In 2021 I Wanna B Doctor',
-    'In 2021 I wanna B Programmer',
-    'In 2021 I Wanna B M',
-    'In 2021 I wanna B T',
-  ];
-  final List<String> times = <String>[
-    'jan - 10 -2020',
-    'In 2021 I Wanna B rich',
-    'In 2021 I wanna B Teacher',
-    'In 2021 I Wanna B Doctor',
-    'In 2021 I wanna B Programmer',
-    'In 2021 I Wanna B M',
-    'In 2021 I wanna B T',
-  ];
+  Widget fun(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12), color: Colors.cyan),
+        height: 200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              child: ListTile(
+                tileColor: Colors.cyan,
+                leading: CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/logo.jpg'),
+                ),
+                title: Text(title),
+                subtitle: Text('Nov - 20 - 2020'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 20, 0, 30),
+              child: Text("What is Yout wish in 2021!!??"),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: SizedBox(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: SizedBox(
+                          child: Row(children: [
+                        Icon(Icons.undo),
+                        Text("Unpublished")
+                      ])),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 40),
+                      child: SizedBox(
+                          child: Row(children: [
+                        Icon(Icons.star_border),
+                        Text("Favorite")
+                      ])),
+                    ),
+                    SizedBox(
+                        child: Row(
+                            children: [Icon(Icons.more_vert), Text("More")])),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
-  // final List<int> colorCodes = <int>[
-  //   600,
-  //   500,
-  //   100,
-  // ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: Names.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.all(5),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.pinkAccent,
-                ),
-                height: 150,
-
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Card(
-                      color: Colors.pinkAccent,
-                      child: ListTile(
-                        leading: FlutterLogo(size: 56.0),
-                        title: Text(Names[index]),
-                        subtitle: Text("Nov/20/2020"),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 10, 0, 30),
-                      child: Text(
-                        wishes[index],
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ),
-                  ],
-                ),
-                // height: 50,
-                // color: Colors.red,
-                // child: Center(child: Text('Entry ${Names[index]}')),
-              ),
-            );
-          }),
+      body: ListView(
+        children: [
+          fun("Tony"),
+          fun("Ameer"),
+          fun("John"),
+          fun("Ali"),
+        ],
+      ),
     );
   }
 }
